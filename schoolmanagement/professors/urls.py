c00dff4b04091ae030ab5993c210db61e482ab45
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-05-27 04:25:56 +0100

    new Commit

--- schoolmanagement/professors/urls.py
+++ schoolmanagement/professors/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('prof-list', views.allProfs),
+    path('', views.allProfs),
+    path('create-prof', views.createProf),
     path('profById/<str:pk>', views.getProfById),
     path('delete-prof/<str:pk>', views.deleteProf)
 ]
