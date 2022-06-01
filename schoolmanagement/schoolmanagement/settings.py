04958ddef5b456669c919808e465757e7c5abdf8
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-06-01 03:47:29 +0100

    new commit

--- schoolmanagement/schoolmanagement/settings.py
+++ schoolmanagement/schoolmanagement/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "students.apps.StudentsConfig",
-    "rest_framework"
+    'students.apps.StudentsConfig',
+
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
