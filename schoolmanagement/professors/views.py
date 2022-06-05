ccc5136e7748a1bbe129d59c132b5cbceb50706a
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-06-05 04:09:23 +0100

    new commit

--- schoolmanagement/professors/views.py
+++ schoolmanagement/professors/views.py
@@ -1,8 +1,15 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Professor
+from .serialezers import ProfessorSerializer
 # Create your views here.
 @api_view(['GET'])
 def profApi(request):
     return Response("Prof Api test")
 
 #GET ALL PROFS :
+def allProfs(request):
+    professors = Professor.objects.all()
+    profsSerializers = ProfessorSerializer(professors, many=True)
+
+    return Response(profsSerializers.data)
