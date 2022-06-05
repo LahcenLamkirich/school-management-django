0ad1bc68deea81f00a6c0cdb644a5b7167f053d3
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-06-05 04:06:58 +0100

    new Commit

--- schoolmanagement/professors/serialezers.py
+++ schoolmanagement/professors/serialezers.py
@@ -2,3 +2,6 @@ from rest_framework import serializers
 
 class ProfessorSerializer(serializers.ModelSerializer):
     first_name = serializers.CharField(max_length=50)
+    last_name = serializers.CharField(max_length=50)
+    matiere = serializers.CharField(max_length=50)
+
