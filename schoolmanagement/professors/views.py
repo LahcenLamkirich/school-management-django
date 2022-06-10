5c1acdb5f0cc617a0765725ed170317ef1f404c2
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-06-10 04:17:03 +0100

    new commit

--- schoolmanagement/professors/views.py
+++ schoolmanagement/professors/views.py
@@ -15,9 +15,18 @@ def allProfs(request):
 
     return Response(profsSerializers.data, status=HTTPStatus.ACCEPTED)
 
+#GET PROF BY ID :
 def getProfById(request, pk):
     prof = Professor.objects.get(id=pk)
     profSer = ProfessorSerializer(prof, many=False)
 
     return Response(profSer.data,status=HTTPStatus.ACCEPTED)
+
+
+def createProf(request):
+    prof = ProfessorSerializer(data=request.data)
+    if prof.is_valid():
+        prof.save()
+
+    return Response(prof.data, status=HTTPStatus.CREATED)
 
