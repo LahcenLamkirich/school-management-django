0b13a084115a6c792957263ca3c44773091cec54
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-06-25 04:36:21 +0100

    new Commit

--- schoolmanagement/departements/models.py
+++ schoolmanagement/departements/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-c
+class Departement(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
