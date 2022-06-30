897a9ca51a63fc5bc9edf714745c5e4be2607b47
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-06-30 03:37:29 +0100

    new commit

--- schoolmanagement/students/urls.py
+++ schoolmanagement/students/urls.py
@@ -3,5 +3,8 @@ from . import views
 # Let's create first the views :
 
 urlpatterns = [
-    path('', views.studentList)
+    path('', views.studentList),
+    path('create-student', views.studentCreate),
+    path('delete-student/<str:pk>', views.deleteStudent),
+    path('studentById/<str:pk>', views.studentById)
 ]
