adc676f048c52944baa8cddfac3aaf60e6816fec
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-06-30 02:02:00 +0100

    new Commit

--- schoolmanagement/students/views.py
+++ schoolmanagement/students/views.py
@@ -1,7 +1,11 @@
+from builtins import id
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Student
 from .serializers import StudentSerializer
+from http import HTTPStatus
+
 # Create your views here:
 
 # GET ALL THE STUDENTS API :
@@ -12,6 +16,18 @@ def studentList(request):
 
     return Response(studentsSerializers.data)
 
-# now we have to create some students : using shell
+#CREATE A STUDENT:
+@api_view(['POST'])
+def studentCreate(request):
+    student = StudentSerializer(data=request.data)
+    if student.is_valid():
+        student.save()
+    return Response(student.data, status=HTTPStatus.CREATED)
 
-GEATTTTTTTTTTTTTTTTT !
+#GET STUDENT BY ID:
+def studentById(request, id):
+    student = Student.objects.get(id=id)
+    studentSeria = StudentSerializer(student, many=False)
+
+    return Response(studentSeria, status=HTTPStatus.ACCEPTED)
+
